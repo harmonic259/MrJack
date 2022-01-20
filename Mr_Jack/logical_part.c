2607befae9688198c4fe527554553080cfe6b876
Amin Shabani <amin.shabani259@gmail.com>
2022-01-20 18:59:40 +0330

    shuffle method and character struct added

--- Mr_Jack/logical_part.c
+++ Mr_Jack/logical_part.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 //struct contains attributes of each cell
 struct cell
 {
@@ -8,9 +10,40 @@ struct cell
     //0: empty/ 1: SH/ 2: JW/ 3: JS/ 4: IL/ 5: MS/ 6: SG/ 7: WG/ 8: JB
     int character;
 };
+
+//struct of characters
+struct character_node
+{
+    int character;
+    struct character_node* next;
+};
+
+
+
 //array of position of each cell for finding them
 struct cell map[13][9];
 
+//swap values of 2 variable
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+// A function to generate a random permutation of arr[]
+void randomize ( int arr[])
+{
+    srand ( time(NULL) );
+
+    for (int i = 7; i > 0; i--)
+    {
+        int j = rand() % (i+1);
+        swap(&arr[i], &arr[j]);
+    }
+}
+
+//initial situation at the beginning of the game
 void init_map()
 {
     FILE* fpin1 = fopen("C:\\Users\\ASUS-pc Ishragh\\Documents\\GitHub\\MrJack\\Mr_Jack\\initial_map_attitude.txt","r");
