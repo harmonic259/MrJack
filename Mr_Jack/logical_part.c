c4eb9f979bb25cbb8f2e85995e17ccbcb2e04fef
Amin Shabani <amin.shabani259@gmail.com>
2022-01-20 16:59:40 +0330

    initial map characters added

--- Mr_Jack/logical_part.c
+++ Mr_Jack/logical_part.c
@@ -3,7 +3,7 @@
 //struct contains attributes of each cell
 struct cell
 {
-    // 0: empty/ 1: home/ 2: opened well/ 3: closed well/ 4: on light/ 5: off light/ 7: unavailable
+    //0: empty/ 1: home/ 2: opened well/ 3: closed well/ 4: on light/ 5: off light/ 7: unavailable
     int attitude;
     //0: empty/ 1: SH/ 2: JW/ 3: JS/ 4: IL/ 5: MS/ 6: SG/ 7: WG/ 8: JB
     int character;
@@ -13,15 +13,24 @@ struct cell map[13][9];
 
 void init_map()
 {
-    FILE* fpin = fopen("C:\\Users\\ASUS-pc Ishragh\\Documents\\GitHub\\MrJack\\Mr_Jack\\initial_map_attitude.txt","r");
+    FILE* fpin1 = fopen("C:\\Users\\ASUS-pc Ishragh\\Documents\\GitHub\\MrJack\\Mr_Jack\\initial_map_attitude.txt","r");
     for(int j = 0; j < 9; j++)
     {
         for(int i=0; i<13; i++)
         {
-            fscanf(fpin, "%d",&(map[i][j].attitude));
+            fscanf(fpin1, "%d",&(map[i][j].attitude));
         }
     }
-    fclose(fpin);
+    fclose(fpin1);
 
+    FILE* fpin2 = fopen("C:\\Users\\ASUS-pc Ishragh\\Documents\\GitHub\\MrJack\\Mr_Jack\\initial_map_character.txt","r");
+    for(int j = 0; j < 9; j++)
+    {
+        for(int i=0; i<13; i++)
+        {
+            fscanf(fpin2, "%d",&(map[i][j].character));
+        }
+    }
+    fclose(fpin2);
 
 }
